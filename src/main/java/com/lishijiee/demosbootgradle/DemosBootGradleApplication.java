67c38edae78dc3b495a2e9cf56e80c7a1d6132bb
lishijiee <2424782003@qq.com>
2022-11-23 20:51:28 +0800

    Initial commit

---
+++ src/main/java/com/lishijiee/demosbootgradle/DemosBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.lishijiee.demosbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosBootGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemosBootGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lishijiee/demosbootgradle/DemosBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lishijiee.demosbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosBootGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
